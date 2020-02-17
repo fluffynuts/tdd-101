ecc881c9b3b6f06cc8e7f24abc377568fcacfb99
Davyd McColl <davydm@gmail.com>
2020-02-17 15:45:28 +0200

    :sparkles: start on a solution for the datetime provider workshop

--- workshops/01-datetime-provider/TestDateTimeProvider.cs
+++ workshops/01-datetime-provider/TestDateTimeProvider.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using NExpect;
 using NUnit.Framework;
 using static NExpect.Expectations;
@@ -16,8 +17,24 @@ namespace TDD101.Workshops.DatetimeProvider
             // Act
             var result = sut.UtcNow;
             // Assert
+            Expect(result)
+                .Not.To.Equal(DateTime.MinValue);
             Expect(result.Kind)
                 .To.Equal(DateTimeKind.Utc);
+        }
+
+        [Test]
+        public void ShouldReturnNewTimeEveryTime()
+        {
+            // Arrange
+            var sut = Create();
+            // Act
+            var result1 = sut.UtcNow;
+            Thread.Sleep(10);
+            var result2 = sut.UtcNow;
+            // Assert
+            Expect(result2)
+                .To.Be.Greater.Than(result1);
         }
 
         [Test]
@@ -27,11 +44,17 @@ namespace TDD101.Workshops.DatetimeProvider
             // Act
             // Assert
         }
-        
+
         private static IDateTimeProvider Create()
         {
-            throw new NotImplementedException();
+            return new DatetimeProviderImpl();
         }
+    }
+
+    internal class DatetimeProviderImpl
+        : IDateTimeProvider
+    {
+        public DateTime UtcNow => DateTime.UtcNow;
     }
 
     public interface IDateTimeProvider
