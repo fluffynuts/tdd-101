32e5c8a825d0e5d14c8c06efe445f516549a860f
Davyd McColl <davydm@gmail.com>
2020-02-25 14:35:33 +0200

    :sparkles: first test

--- workshops/02-string-calculator/TestStringCalculator.cs
+++ workshops/02-string-calculator/TestStringCalculator.cs
@@ -1,14 +1,38 @@
 using System;
 using NUnit.Framework;
+using NExpect;
+using static NExpect.Expectations;
 
 namespace TDD101.Workshops.StringCalculator
 {
     [TestFixture]
     public class TestStringCalculator
     {
+        [Test]
+        public void ShouldReturnZeroForEmptyString()
+        {
+            // Arrange
+            var input = "";
+            var sut = Create();
+            // Act
+            var result = sut.Add(input);
+            // Assert
+            Expect(result)
+                .To.Equal(0);
+        }
+        
         private static IStringCalculator Create()
         {
-            throw new NotImplementedException();
+            return new StringCalculatorImpl();
+        }
+    }
+
+    internal class StringCalculatorImpl
+        : IStringCalculator
+    {
+        public int Add(string input)
+        {
+            return 0;
         }
     }
 
