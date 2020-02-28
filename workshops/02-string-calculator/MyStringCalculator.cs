f32226a7d96c85f176cee09812f0a08a151e3fba
Davyd McColl <davydm@gmail.com>
2020-02-28 16:23:07 +0200

    :memo: fix doc formatting

---
+++ workshops/02-string-calculator/MyStringCalculator.cs
@@ -0,0 +1,11 @@
+namespace TDD101.Workshops.StringCalculator
+{
+    internal class MyStringCalculator
+        : IStringCalculator
+    {
+        public int Add(string input)
+        {
+            return 0;
+        }
+    }
+}
